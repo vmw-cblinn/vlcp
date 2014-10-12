a0e45caf0833ece5a04dd1c1406ea45b6b94d4d3
Chris Blinn <cblinn@vmware.com>
2014-10-12 15:25:16 -0700

    New version of generate-index.py that creates different versions of content packs
    Also add framework information to all content packs for better
    integration with the script

--- generate-index.py
+++ generate-index.py
@@ -2,23 +2,82 @@
 
 import json
 import os
+import sys
 
-baseDir = os.path.dirname(os.path.realpath(__file__))
-contentDir = os.path.join(baseDir, 'content')
-jsonFile = os.path.join(baseDir, 'index.json');
-cps = []
-for fileName in os.listdir(contentDir):
-    if '.vlcp' == os.path.splitext(fileName)[1]:
-        f = open(os.path.join(contentDir, fileName))
-        text = f.read();
-        js = json.loads(text);
-        js.pop('dashboardSections', None)
-        js.pop('alerts', None)
-        js.pop('queries', None)
-        js.pop('eventTypes', None)
-        js.pop('extractedFields', None)
-        js['fileName'] = fileName
-        cps.append(js)
-f = open(jsonFile, 'w')
-f.write(json.dumps(cps, indent=2, separators=(',', ': ')))
-f.close()
+base_dir = os.path.dirname(os.path.realpath(__file__))
+content_dir = os.path.join(base_dir, 'content/')
+json_file = os.path.join(base_dir, 'index.json')
+
+def prep_json(file_path):
+    js = load_json(file_path)
+    js.pop('dashboardSections', None)
+    js.pop('alerts', None)
+    js.pop('queries', None)
+    js.pop('eventTypes', None)
+    js.pop('extractedFields', None)
+    js['fileName'] = prep_path(file_path)
+    return js
+
+
+def load_json(file_path):
+    f = open(file_path)
+    text = f.read()
+    f.close()
+    return json.loads(text)
+
+
+def prep_path(file_path):
+    return file_path.split(content_dir)[1]
+
+
+def hex_name(framework):
+    return "#" +  str(hex(framework))[2:]
+
+
+frameworks = {}
+paths = []
+for root, subs, names in os.walk(content_dir):
+    for name in names:
+        if name.endswith('.vlcp'):
+            path = os.path.join(root, name)
+            js = load_json(path)
+            framework = int(js['framework'][1:], 16)
+            namespace = js['namespace']
+
+            if framework not in frameworks:
+                frameworks[framework] = {}
+            if namespace in frameworks[framework]:
+                print "The following two files contain the same namespace and framemwork:"
+                print "\t" + prep_path(path)
+                print "\t" + prep_path(namespaces[namespace][framework])
+                print "Remove the older version and rerun this script to continue"
+                sys.exit()
+            frameworks[framework][namespace] = path
+            paths.append(path)
+
+versions = sorted(frameworks.keys())
+index_files = {}
+# since content packs are backwards compatible
+# push all previous versions to the future index files
+# eg, for all framework 1000 packs will appear in 1500+
+index_files[versions[0]] = []
+for i in range(1, len(versions)):
+    curr = versions[i]
+    prev = versions[i - 1]
+    index_files[curr] = []
+    for namespace in frameworks[prev]:
+        if namespace not in frameworks[curr]:
+            frameworks[curr][namespace] = frameworks[prev][namespace]
+
+for path in paths:
+    js = prep_json(path)
+    framework = int(js['framework'][1:], 16)
+    namespace = js['namespace']
+    for i in range(versions.index(framework), len(versions)):
+        if frameworks[versions[i]][namespace] == path:
+            index_files[versions[i]].append(js)
+
+for index in index_files:
+    g = open(json_file + hex_name(index), 'w')
+    g.write(json.dumps(index_files[index], indent=2, separators=(',', ': ')))
+    g.close()
